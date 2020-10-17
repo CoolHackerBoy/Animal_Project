d11d698120c70fe2b768da24a7e8db0913cb52c5
CoolHackerBoy <65199906+CoolHackerBoy@users.noreply.github.com>
2020-10-17 14:12:59 -0700

    Huge Update
    I made the homepage look more organized.  I added what animals im going to talk about in the info about animals.  I also made it that if you hover your mouse over an animal in the info about animals, the blur goes away for that animal.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+$(document).on('click','button#backButton',function(){
+	window.location ="index.html"
+});
+
+$(document).on('click','button#infoButton',function(){
+	window.location="info.html";
+});
 $(document).on('click','button#infoButton',function(){
 	window.location="info.html";
 });
@@ -10,8 +17,21 @@ $(document).on('click','button#creditButton',function(){
 	window.location="credit.html";
 });
 
+$(document).on('click','button#button1',function(){
+	$('div#animalPick').hide();
+	$('button#backButton').show();
+});
 
+$(document).on('click','button#button2',function(){
+	$('div#animalPick').hide();
+	$('button#backButton').show();
+});
+
+$(document).on('click','button#button3',function(){
+	$('div#animalPick').hide();
+	$('button#backButton').show();
+});
 
 $(document).ready(function(){
-	
+
 });
