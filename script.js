e233d5240c3e6c87646a8af0eca8eebaa2385161
CoolHackerBoy <65199906+CoolHackerBoy@users.noreply.github.com>
2020-10-18 12:37:56 -0700

    Finish
    Added facts about the animals.  Also, added more buttons in the credits.

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ $(document).on('click','button#pickAnotherAnimal',function(){
 	$('div#animalPick').show();
 	$('button#pickAnotherAnimal').hide();
 	$('button#backButton').show();
+	$('div#rhino').hide();
+	$('div#greyWhale').hide();
+	$('div#polarBear').hide();
 })
 
 $(document).on('click','button#infoButton',function(){
@@ -44,24 +47,42 @@ $(document).on('click','button#MagdaEhlers',function(){
 	window.location ="https://www.pexels.com/video/sea-otters-swimming-2554522/"
 });
 
+$(document).on('click','button#rhinoFacts',function(){
+	window.location="https://www.livescience.com/27439-rhinos.html#:~:text=White%20rhinos%20and%20black%20rhinos,Indonesian%20swamps%20and%20rain%20forests."
+})
+
+$(document).on('click','button#greyWhaleFacts',function(){
+	window.location="https://www.fisheries.noaa.gov/species/gray-whale#:~:text=Gray%20whales%20are%20found%20mainly,the%20coast%20of%20eastern%20Asia."
+})
+
+$(document).on('click','button#polarBearFacts',function(){
+	window.location="https://www.nwf.org/Educational-Resources/Wildlife-Guide/Mammals/Polar-Bear#:~:text=They%20mainly%20eat%20ringed%20seals,eat%20walruses%20and%20whale%20carcasses."
+})
+
 $(document).on('click','button#button1',function(){
 	$('div#animalPick').hide();
 	$('button#backButton').hide();
 	$('button#pickAnotherAnimal').show();
+	$('div#rhino').show();
 });
 
 $(document).on('click','button#button2',function(){
 	$('div#animalPick').hide();
 	$('button#backButton').hide();
 	$('button#pickAnotherAnimal').show();
+	$('div#greyWhale').show();
 });
 
 $(document).on('click','button#button3',function(){
 	$('div#animalPick').hide();
 	$('button#backButton').hide();
 	$('button#pickAnotherAnimal').show();
+	$('div#polarBear').show();
 });
 
 $(document).ready(function(){
 	$('button#pickAnotherAnimal').hide();
+	$('div#rhino').hide();
+	$('div#greyWhale').hide();
+	$('div#polarBear').hide();
 });
